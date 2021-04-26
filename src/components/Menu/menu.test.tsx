413175f3ca3beed5b49d0435330d76e510527a6d
zhangluJs <897268181@qq.com>
2021-04-26 11:31:03 +0800

    feat: add submenu test

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -12,7 +12,8 @@ const testProps: MenuProps = {
 
 const testVerProps: MenuProps = {
     defaultIndex: '0',
-    mode: 'vertical'
+    mode: 'vertical',
+    defaultSubMenus: ['4']
 }
 
 /**
@@ -26,7 +27,10 @@ const generateMenu = (props: MenuProps) => {
             <MenuItem index="1" disabled>disabled</MenuItem>
             <MenuItem index="2">xyz</MenuItem>
             <SubMenu title="dropdown" index="3">
-                <MenuItem index="3-1">drop1</MenuItem>
+                <MenuItem index="3-0">drop1</MenuItem>
+            </SubMenu>
+            <SubMenu title="opened" index="4">
+                <MenuItem index="4-0">opened1</MenuItem>
             </SubMenu>
         </Menu>
     );
@@ -37,8 +41,8 @@ const createStyleFile = () => {
         .lu-submenu {
             display: none;
         }
-        .lu-submenu.menu-open {
-            display: blobk;
+        .lu-submenu.menu-opened {
+            display: block;
         }
     `;
     const style = document.createElement('style');
@@ -47,7 +51,7 @@ const createStyleFile = () => {
     return style;
 }
 
-let wrapper: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
+let wrapper: RenderResult, wrapper2: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
 
 describe('test Menu and MenuItem components', () => {
     // beforeEach 是每个case执行前都会跑的函数
@@ -78,7 +82,7 @@ describe('test Menu and MenuItem components', () => {
          * :scope 是一个伪类，代表了当前节点的本身
          * https://developer.mozilla.org/zh-CN/docs/Web/CSS/:scope 解释的很详细
          */
-        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
+        expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5);
         expect(activeElement).toHaveClass('is-actived menu-item');
         expect(disabledElement).toHaveClass('is-disabled menu-item');
     })
@@ -106,7 +110,7 @@ describe('test Menu and MenuItem components', () => {
 
     it('should show dropdown items when hover on subMenu', async () => {
         /**
-         * 为这里添加了一些css。因为没有css样式的话，这里的测试用例会认为当前节点其实是存在的。不过也缺失存在
+         * 为这里添加了一些css。因为没有css样式的话，这里的测试用例会认为当前节点其实是存在的。不过也确实存在，所以要加css
          */
         expect(wrapper.queryByText('drop1')).not.toBeVisible();
         const dropdownElement = wrapper.getByText('dropdown');
@@ -114,5 +118,33 @@ describe('test Menu and MenuItem components', () => {
         await waitFor(() => {
             expect(wrapper.queryByText('drop1')).toBeVisible();
         });
+        fireEvent.click(wrapper.getByText('drop1'));
+        expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+        fireEvent.mouseLeave(dropdownElement);
+        await waitFor(() => {
+            expect(wrapper.queryByText('drop1')).not.toBeVisible();
+        });
+    })
+})
+
+describe('test Menu and MenuItem component in vertical mode', () => {
+    beforeEach(() => {
+        // 渲染组件
+        wrapper2 = render(generateMenu(testVerProps));
+        
+        wrapper2.container.append(createStyleFile());
+    })
+    it ('should render vertical mode when mode is set to vertical', () => {
+        const menuElement = wrapper2.getByTestId('test-menu');
+        expect(menuElement).toHaveClass('menu-vertical')
+    })
+    it('should show dropdown items when click on subMenu for vertical mode', () => {
+        expect(wrapper.queryByText('drop1')).not.toBeVisible();
+        const dropdownElement = wrapper2.getByText('dropdown');
+        fireEvent.click(dropdownElement);
+        expect(wrapper.queryByText('drop1')).toBeVisible();
+    })
+    it('should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index', () => {
+        expect(wrapper2.queryByText('opened1')).toBeVisible()
     })
 })
