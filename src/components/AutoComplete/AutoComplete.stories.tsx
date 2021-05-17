92fcf51183c0f783af6addb7a7deb7bcf1333b16
zhangluJs <897268181@qq.com>
2021-05-17 15:26:42 +0800

    feat: AutoComplete 组件添加自定义渲染规则&接收数据类型调整为复杂类型

--- src/components/AutoComplete/AutoComplete.stories.tsx
+++ src/components/AutoComplete/AutoComplete.stories.tsx
@@ -1,16 +1,34 @@
 import React, { ChangeEvent, useState } from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
-import AutoComplete, {AutoCompleteProps} from './AutoComplete';
+import AutoComplete, {AutoCompleteProps, DataSourceType} from './AutoComplete';
 import Input, {InputProps} from '../Input/Input';
 
-const defaultInput = () => {
+const lakersWithNumber = [
+    {value: 'bradley', number: 11},
+    {value: 'pope', number: 1},
+    {value: 'caruso', number: 4},
+    {value: 'cook', number: 2},
+    {value: 'cousins', number: 15},
+    {value: 'james', number: 23},
+    {value: 'AD', number: 3},
+    {value: 'green', number: 14},
+    {value: 'howard', number: 39},
+    {value: 'kuzma', number: 0},
+];
+
+interface lakersPropsValue {
+    value: string,
+    number: number
+}
+
+const simpleComplete = () => {
     const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
   'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando'];
     const handleFetch = (value: string) => {
-        return lakers.filter(item => item.includes(value));
+        return lakers.filter(item => item.includes(value)).map(item => ({value: item}));
     }
-    return(
+    return (
         <AutoComplete
             fetchSuggestions={handleFetch}
             onSelect={action('click item')}
@@ -18,5 +36,25 @@ const defaultInput = () => {
     )
 }
 
+const renderTemplate = () => {
+    const handleFetch = (value: string) => {
+        return lakersWithNumber.filter(item => item.value.includes(value));
+    }
+    const handleRender = (item: DataSourceType) => {
+        const itemWithNumber = item as DataSourceType<lakersPropsValue>;
+        return (
+            <h5>Name: {itemWithNumber.value} {itemWithNumber.number}</h5>
+        )
+    }
+    return (
+        <AutoComplete
+            fetchSuggestions={handleFetch}
+            onSelect={action('click item')}
+            renderOption={handleRender}
+        />
+    )
+}
+
 storiesOf('AutoComplete', module)
-    .add('AutoComplete', defaultInput)
+    .add('AutoComplete', simpleComplete)
+    .add('customTemplate', renderTemplate)
--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,9 +1,22 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import Input, {InputProps} from '../Input/Input';
 
+/**
+ * 因为有可能接收更复杂的数据类型，不能item只是一个string
+ * 所以需要将一开始定义的string类型全部更换为复杂的类型（object）
+ * 
+ */
+interface DataSuorctObject {
+    value: string
+}
+
+// 复杂类型
+export type DataSourceType<T = {}> = T & DataSuorctObject;
+
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-    fetchSuggestions: (str: string) => string[];
-    onSelect?: (item: string) => void;
+    fetchSuggestions: (str: string) => DataSourceType[];
+    onSelect?: (item: DataSourceType) => void;
+    renderOption?: (item: DataSourceType) => ReactElement;
 }
 
 const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
@@ -11,13 +24,14 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
         fetchSuggestions,
         onSelect,
         value,
+        renderOption,
         ...restPorps
     } = props;
 
     // 初始化input的value
     const [inputValue, setInputValue] = useState(value);
     // 用来渲染联系下拉list的数组
-    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value.trim();
@@ -30,12 +44,16 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
         }
     }
 
-    const handleSelect = (item: string) => {
-        setInputValue(item);
+    const handleSelect = (item: DataSourceType) => {
+        setInputValue(item.value);
         setSuggestions([]);
         if (onSelect) {
             onSelect(item);
         }
+    }
+
+    const renderTemplate = (item: DataSourceType) => {
+        return renderOption? renderOption(item) : item.value;
     }
 
     const generateDropdown = () => {
@@ -45,13 +63,13 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
                    suggestions.map((item, index) => {
                        return (
                            <li key={index} onClick={() => {handleSelect(item)}}>
-                               {item}
+                               {renderTemplate(item)}
                            </li>
-                       )
-                   }) 
+                       );
+                   })
                 }
             </ul>
-        )
+        );
     }
 
     return (
@@ -62,7 +80,7 @@ const AutoComplete: React.FC<AutoCompleteProps> = (props) => {
                 {...restPorps}></Input>
             {(suggestions.length > 0) && generateDropdown()}
         </div>
-    )
+    );
 }
 
 export default AutoComplete;
